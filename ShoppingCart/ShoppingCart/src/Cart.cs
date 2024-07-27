088f3c31356e925e139e38fa75705c9df1315239
Yu-Lun <chou522128@gmail.com>
2024-07-27 22:27:36 +0800

    [Refactor] Cart.cs：introduce totalProducts,totalPrice variable

--- ShoppingCart/ShoppingCart/src/Cart.cs
+++ ShoppingCart/ShoppingCart/src/Cart.cs
@@ -4,14 +4,16 @@
     {
         public string printShoppingCart()
         {
+            var totalProducts = 0;
+            var totalPrice = 0;
             return "-----------------------------------------\n" +
                 "| Product    | Price      | Quantity    |\n" +
                 "| ---------- | ---------- | ----------- |\n" +
                 "|---------------------------------------|\n" +
                 "| Promotion:                            |\n" +
                 "|---------------------------------------|\n" +
-                "| Total products: 0                     |\n" +
-                "| Total price: 0.00 €                   |\n" +
+               $"| Total products: {totalProducts}                     |\n" +
+               $"| Total price: {totalPrice.ToString("F2")} €                   |\n" +
                 "-----------------------------------------";
         }
     }
