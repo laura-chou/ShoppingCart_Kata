8d2cf327cbfcec4390c06ea6090477c3732d245c
Yu-Lun <chou522128@gmail.com>
2024-07-27 23:06:48 +0800

    [Green]  check cart have product and hard code return

--- ShoppingCart/ShoppingCart/src/Cart.cs
+++ ShoppingCart/ShoppingCart/src/Cart.cs
@@ -1,11 +1,33 @@
-﻿namespace ShoppingCart.src
+﻿using System.Xml.Linq;
+
+namespace ShoppingCart.src
 {
     public class Cart
     {
+        private List<Product> _cart;
+
+        public Cart()
+        {
+            _cart = new List<Product>();
+        }
+
         public string printShoppingCart()
         {
-            var totalProducts = 0;
+            var totalProducts = _cart.Count;
             var totalPrice = 0;
+            if (_cart.Count > 0)
+            {
+                return "-----------------------------------------\n" +
+                "| Product    | Price      | Quantity    |\n" +
+                "| ---------- | ---------- | ----------- |\n" +
+                "| Iceberg    | 2.17 €     | 1           |\n" +
+                "|---------------------------------------|\n" +
+                "| Promotion:                            |\n" +
+                "|---------------------------------------|\n" +
+                "| Total products: 1                     |\n" +
+                "| Total price: 2.17 €                   |\n" +
+                "-----------------------------------------";
+            }
             return "-----------------------------------------\n" +
                 "| Product    | Price      | Quantity    |\n" +
                 "| ---------- | ---------- | ----------- |\n" +
@@ -17,9 +39,14 @@
                 "-----------------------------------------";
         }
 
-        internal void addItem(string v1, int v2)
+        public void addItem(string productName, int quantity)
         {
-            throw new NotImplementedException();
+            _cart.Add(new Product
+            {
+                Name = productName,
+                Price = 1.79,
+                Quantity = quantity
+            });
         }
     }
 }
---
+++ ShoppingCart/ShoppingCart/src/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingCart.src
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
