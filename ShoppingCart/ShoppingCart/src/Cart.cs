7d90cd7d173a59f534c36dba94dba84958156c06
Yu-Lun <chou522128@gmail.com>
2024-08-04 00:13:48 +0800

    [Refactor] use template to print result

--- ShoppingCart/ShoppingCart/src/Cart.cs
+++ ShoppingCart/ShoppingCart/src/Cart.cs
@@ -1,50 +1,33 @@
-﻿using System.Xml.Linq;
+﻿using System.Text;
+using System.Xml.Linq;
 
 namespace ShoppingCart.src
 {
     public class Cart
     {
+        internal string printTemplate = @"
+            -----------------------------------------
+            | Product    | Price      | Quantity    |
+            | ---------- | ---------- | ----------- |
+            {ProductRows}
+            | Promotion: {Promotion}                           |
+            |---------------------------------------|
+            | Total products: {TotalProducts}                     |
+            | Total price: {TotalPrice} €                   |
+            -----------------------------------------";
+
         private List<Product> _cart;
         private Product _product;
-
         public Cart()
         {
             _cart = new List<Product>();
             _product = new Product();
         }
 
-        public string printShoppingCart()
-        {
-            var totalProducts = _cart.Count;
-            var totalPrice = 0;
-            if (_cart.Count > 0)
-            {
-                return "-----------------------------------------\n" +
-                "| Product    | Price      | Quantity    |\n" +
-                "| ---------- | ---------- | ----------- |\n" +
-                "| Iceberg    | 2.17 €     | 1           |\n" +
-                "|---------------------------------------|\n" +
-                "| Promotion:                            |\n" +
-                "|---------------------------------------|\n" +
-                "| Total products: 1                     |\n" +
-                "| Total price: 2.17 €                   |\n" +
-                "-----------------------------------------";
-            }
-            return "-----------------------------------------\n" +
-                "| Product    | Price      | Quantity    |\n" +
-                "| ---------- | ---------- | ----------- |\n" +
-                "|---------------------------------------|\n" +
-                "| Promotion:                            |\n" +
-                "|---------------------------------------|\n" +
-               $"| Total products: {totalProducts}                     |\n" +
-               $"| Total price: {totalPrice.ToString("F2")} €                   |\n" +
-                "-----------------------------------------";
-        }
-
         public void addItem(string productName, int quantity)
         {
             var product = _product.getProduct(productName);
-            
+
             if (product != null)
             {
                 _cart.Add(new Product
@@ -54,6 +37,49 @@ namespace ShoppingCart.src
                     Quantity = quantity
                 });
             }
+        }
+
+        public string printShoppingCart()
+        {
+            var totalProducts = _cart.Sum(p => p.Quantity);
+            var totalPrice = _cart.Sum(p => p.Price);
+            string productRows = BuildProductRows();
+
+
+            return ReplacePlaceholders(productRows, totalProducts, totalPrice);
+        }
+
+        private string BuildProductRows()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var product in _cart)
+            {
+                var price = product.Price * product.Quantity;
+                sb.AppendLine($"| {product.Name.PadRight(10)} | {$"{price.ToString("F2")} €".PadRight(10)} | {product.Quantity.ToString().PadRight(11)} |");
+                sb.Append(string.Empty.PadRight(12));
+            }
+
+            sb.Append("|---------------------------------------|");
+            return sb.ToString();
+        }
+        
+        private string ReplacePlaceholders(string productRows, int totalProducts, double totalPrice)
+        {
+            var replacements = new Dictionary<string, string>
+            {
+                { "ProductRows", productRows },
+                { "Promotion", string.Empty },
+                { "TotalProducts", totalProducts.ToString() },
+                { "TotalPrice", totalPrice.ToString("F2") }
+            };
+
+            foreach (var item in replacements)
+            {
+                string placeholder = $"{{{item.Key}}}";
+                printTemplate = printTemplate.Replace(placeholder, item.Value);
+            }
+
+            return printTemplate;
         }
     }
 }
