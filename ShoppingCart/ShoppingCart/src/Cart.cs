158f45de6dfb8ca8f6bcd9d3b369e68a9120da8f
Yu-Lun <chou522128@gmail.com>
2024-10-26 23:24:02 +0800

    [Green] Cart.cs：applyDiscount hard code and get TotalPrice from CalculatePrice method

--- ShoppingCart/ShoppingCart/src/Cart.cs
+++ ShoppingCart/ShoppingCart/src/Cart.cs
@@ -1,19 +1,23 @@
-﻿namespace ShoppingCart.src
+﻿using FluentAssertions.Equivalency;
+
+namespace ShoppingCart.src
 {
     public class Cart
     {
         public List<Product>? Products = new List<Product>();
-        private Product _product { get; set; }
-        public double TotalPrice => Products.Sum(product => product.Price);
-
-        public Cart() { 
+        public Cart()
+        {
             _product = new Product();
         }
 
+        public Discount Promotion { get; set; }
+        public double TotalPrice => CalculatePrice();
+        private Product _product { get; set; }
+        
         public void addItem(string product, int quantity)
         {
             var price = _product.getProductPrice(product);
-            
+
             if (price > 0 && quantity > 0)
             {
                 Products.Add(new Product
@@ -25,10 +29,19 @@
             }
         }
 
+        public void applyDiscount(string discount)
+        {
+            Promotion = new Discount
+            {
+                Code = discount,
+                Amount = 0.05
+            };
+        }
+
         public void deleteItem(string product, int quantity)
         {
             var item = Products.FirstOrDefault(p => p.Name == product);
-            
+
             if (item != null)
             {
                 item.Quantity -= quantity;
@@ -36,12 +49,19 @@
                 if (item.Quantity <= 0)
                 {
                     Products.Remove(item);
-                } 
+                }
                 else
                 {
                     item.Price = _product.getProductPrice(item.Name) * item.Quantity;
                 }
             }
+        }
+
+        private double CalculatePrice()
+        {
+            var price = Products.Sum(product => product.Price);
+            var discount = price * Promotion?.Amount;
+            return discount != null ? Math.Round((double)(price - discount), 2) : price;
         }
     }
 }
---
+++ ShoppingCart/ShoppingCart/src/Discount.cs
@@ -0,0 +1,8 @@
+﻿namespace ShoppingCart.src
+{
+    public class Discount
+    {
+        public string Code { get; set; }
+        public double Amount { get; set; }
+    }
+}
